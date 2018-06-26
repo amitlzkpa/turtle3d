3d0cea92503f354d389f6136b1fed37c97a23562
Amit Nambiar <amit.lzkpa@gmail.com>
2018-06-26 18:41:49 -0400

    single page routing

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,18 @@
 window.addEventListener('load', () => {
 	const el = $('#app');
+	const errorTemplate = Handlebars.compile($('#error-template').html());
 
-	el.append("Hi there!");
+	const router = new Router({
+		mode: 'history',
+		page404: (path) => {
+			const html = errorTemplate({
+				title: 'Error 404 - Page NOT Found!',
+				message: `The path '/${path}' does not exist on this site`,
+			});
+			el.html(html);
+		},
+	});
+
+	router.navigateTo('');
+	el.html("Hi there!");
 });
