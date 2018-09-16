1f36313898cb55ea142f1bc1652200a78766fdd3
Amit Nambiar <amit.lzkpa@gmail.com>
2018-09-16 16:03:41 -0400

    make axiom procedural; pass recursion level and parent to ruleset generators; add orbit controls;

--- public/js/app.js
+++ public/js/app.js
@@ -38,7 +38,7 @@ function LState() {
 	this.init = function(axiom, ruleSet, constants) {
 		this.constants = constants;
 		this.iters = 0;
-		this.state = axiom;
+		this.state = axiom();
 		this.ruleSet = ruleSet;
 	}
 
@@ -47,7 +47,7 @@ function LState() {
 		let stateBuffer = [];
 		for (let i = 0; i < this.state.length; i++) {
 			let o = this.state[i];
-			stateBuffer.push(...this.ruleSet[o]());
+			stateBuffer.push(...this.ruleSet[o.lid](this.iters, o));
 		}
 		this.state = stateBuffer;
 		this.iters++;
@@ -63,17 +63,42 @@ function LState() {
 
 
 function ABASys() {
-	this.axiom = ["A"];
+	this.axiom = function() { return ["A"] };
 	this.ruleSet = 	{
-						"A": function() { return ["A", "B", "A"]},
-						"B": function() { return ["B", "B", "B"]}
+						"A": function() { return ["A", "B", "A"] },
+						"B": function() { return ["B", "B", "B"] }
 					}
 }
 
 
 function ThreeBasicSys() {
-	this.axiom = ["A"];
-	this.ruleSet = {"A": ["A", "B", "A"], "B": ["B", "B", "B"]};
+	var m = new THREE.MeshNormalMaterial({color: 0xfbfcaf});
+
+
+	this.axiom = () => 	{
+							let g = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+						    let c = new THREE.Mesh( g, this.m );
+						    c.lid = "cube";
+							return [c];
+						};
+
+	this.ruleSet = {};
+	this.ruleSet["cube"] = (i, parent) => 	{
+										let r = [];
+										let g = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+									    let c = new THREE.Mesh( g, this.m );
+									    c.position.x = parent.position.x + i/10;
+									    c.position.z = parent.position.z - i/10;
+										c.lid = "cube";
+									    r.push(c);
+										g = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+									    c = new THREE.Mesh( g, this.m );
+									    c.position.x = parent.position.x - i/10;
+									    c.position.z = parent.position.z + i/10;
+										c.lid = "cube";
+									    r.push(c);
+										return r;
+									}
 }
 
 
@@ -82,8 +107,9 @@ function ThreeBasicSys() {
 function THREEapp() {
 
 	let camera, scene, renderer;
+	let controls;
 
-	let lsys;
+	let lsys, lobjs;
 	 
 	init();
 	animate();
@@ -92,31 +118,55 @@ function THREEapp() {
 		camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
 		camera.position.z = 1;
 		scene = new THREE.Scene();
+		controls = new THREE.OrbitControls( camera );
 
+		lobjs = new THREE.Object3D();
 		lsys = new LState();
-		let rSet = new ABASys();
+		let rSet = new ThreeBasicSys();
 		lsys.init(rSet.axiom, rSet.ruleSet);
 
 		renderer = new THREE.WebGLRenderer( { antialias: true } );
 		renderer.setSize( window.innerWidth, window.innerHeight );
+		renderer.setClearColor(0xffffff);
+		controls.update();
 		document.body.appendChild( renderer.domElement );
 	}
 	 
 	function animate() {
-	    requestAnimationFrame( animate );	 
+	    requestAnimationFrame( animate );
+	    controls.update();	 
 	    renderer.render( scene, camera );
 	 
 	}
 
 
 	async function step() {
+		scene.remove(lobjs);
+		lobjs = new THREE.Object3D();
 		await lsys.step();
-		console.log(lsys.state);
+		for (let i = 0; i < lsys.state.length; i++) {
+			lobjs.add(lsys.state[i]);
+		}
+		scene.add(lobjs);
+		// console.log(lobjs);
 	}
 
 
-	$(document).click(function() {
-	    step();
+	$(document).mousedown(function() {
+	    switch (event.which) {
+	        case 1:
+	            // alert('Left Mouse button pressed.');
+	            break;
+	        case 2:
+	            // alert('Middle Mouse button pressed.');
+	            break;
+	        case 3:
+	        	step();
+	            // alert('Right Mouse button pressed.');
+	            break;
+	        default:
+	            alert('You have a strange Mouse!');
+	    }
 	});
 
 }
