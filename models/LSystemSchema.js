3394367f94efc1083d67bd9a159930e1f8039333
Amit Nambiar <amit.lzkpa@gmail.com>
2018-10-08 21:46:34 -0400

    serve saved data from database

--- models/LSystemSchema.js
+++ models/LSystemSchema.js
@@ -5,17 +5,18 @@ const mongoose = require('mongoose')
 
 let LSystemSchema = new mongoose.Schema(
     {
-        name: { type: String, required: true },
+        name: { type: String, required: true, unique: true },
         author: { type: String },
-        description: { type: String }, 
-        turtles: { type: String }, 
-        rulesets: { type: String }, 
+        description: { type: String },
+        helpers: [{ type: String }],
+        turtles: [{ type: String }], 
+        rulesets: [{ type: String }], 
         created: { type: Date, default: Date.now },
     },
     {
         timestamps: true
     }
-)
+);
 
 
 
