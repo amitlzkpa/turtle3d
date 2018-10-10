325e648f334a71c08ec4aedfebd2cf6942342377
Amit Nambiar <amit.lzkpa@gmail.com>
2018-10-10 01:26:25 -0400

    gabriels world setup

--- public/js/app.js
+++ public/js/app.js
@@ -31,62 +31,124 @@ function getPointInBetweenByLen(pointA, pointB, percentage) {
     return pointA.clone().add(dir);
 }
 
+function getRandomPosNegFloat(max) {
+	return (Math.random()*max) * ((Math.random() < 0.5) ? 1 : -1);
+}
 
-function getR(r) {
-	return (Math.random()*r) * ((Math.random() < 0.5) ? 1 : -1);
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
 }
 
 
 // -----------------------------------------
 
 
-function LineTurtle(start, end) {
-	let g, c;
-	let m = new THREE.LineBasicMaterial({ color: 0x000000 });
-	g = new THREE.Geometry();
-	g.vertices.push(start, end);
-    c = new THREE.Line( g, m );
-    c.name = "line";
-    c.start =  () => { return g.vertices[0]; }
-    c.end = () => { return g.vertices[1]; }
-    return c;
+function getDwellingSphere(i) {
+	let szs = [0.03, 0.04, 0.05];
+	let divss = [4, 5, 7];
+	let  sz = szs[i];
+	let divs = divss[i];
+	let g, m, w, o;
+	o = new THREE.Object3D();
+	g = new THREE.SphereGeometry( sz, divs, divs );
+	let reg_mat = new THREE.MeshBasicMaterial({ color: Math.random() * 0xffffff  });
+    m = new THREE.Mesh( g, reg_mat );
+    let wire_mat = new THREE.LineBasicMaterial( { color: 0x000000, linewidth: 4 } );
+    w = new THREE.LineSegments( g, wire_mat );
+    o.add(m);
+    o.add(w);
+    o.rotation.set(Math.random(), Math.random(), Math.random());
+    return o;
 }
 
 
-function BubbleLineTurtle(start, end) {
-	let os = new THREE.Object3D();
-	let g, c;
-	let m = new THREE.LineBasicMaterial({ color: 0x000000 });
-	g = new THREE.Geometry();
-	let mid = getPointInBetweenByLen(start, end, 0.5);
-	let d = end.distanceTo(start);
-	let r = 0.2 * d;
-	g.vertices.push(start, end);
-    os.add(new THREE.Line( g, m ));
-    os.name = "line";
-    os.start =  () => { return g.vertices[0]; }
-    os.end = () => { return g.vertices[1]; }
-    let sphere = new THREE.Mesh( new THREE.SphereGeometry( r, 16, 16 ), new THREE.MeshBasicMaterial( {color: 0xffffff * Math.random()} ) );
-    sphere.position.set(mid.x, mid.y, mid.z);
-    os.add(sphere);
-    return os;
+function Dwelling_3(start, end) {
+	let s = start;
+	let e = end;
+	let o = new THREE.Object3D();
+	let i;
+	let moveRMax = 0.1;
+	i = getDwellingSphere(getRandomInt(1));
+	i.position.set(end.x + getRandomPosNegFloat(moveRMax), end.y + getRandomPosNegFloat(moveRMax*0.07), end.z + getRandomPosNegFloat(moveRMax));
+    o.add(i);
+	i = getDwellingSphere(getRandomInt(2));
+	i.position.set(end.x + getRandomPosNegFloat(moveRMax), end.y + getRandomPosNegFloat(moveRMax*0.07), end.z + getRandomPosNegFloat(moveRMax));
+    o.add(i);
+	i = getDwellingSphere(getRandomInt(2));
+	i.position.set(end.x + getRandomPosNegFloat(moveRMax), end.y + getRandomPosNegFloat(moveRMax*0.07), end.z + getRandomPosNegFloat(moveRMax));
+    o.add(i);
+	let g = new THREE.Geometry();
+	g.vertices.push(s, e);
+    let c = new THREE.Line( g, new THREE.LineBasicMaterial({ color: 0x000000 }) );
+    o.add(c);
+    o.name = "Dwelling_4";
+    o.start =  () => { return s; }
+    o.end = () => { return e; }
+    return o;
 }
 
 
-function StiltedTurtle(start, end) {
-	let g, c;
-	let m = new THREE.LineBasicMaterial({ color: 0x000000 });
-	g = new THREE.Geometry();
-	let mid = getPointInBetweenByLen(start, end, 0.5);
-	let d = end.distanceTo(start);
-	let r = 0.3 * d;
-	mid.add(new THREE.Vector3(getR(r), getR(r), getR(r)));
-	g.vertices.push(start, mid, end);
-    c = new THREE.Line( g, m );
-    c.name = "line";
-    c.start =  () => { return g.vertices[0]; }
-    c.end = () => { return g.vertices[2]; }
-    return c;
+function Dwelling_4(start, end) {
+	let s = start;
+	let e = end;
+	let o = new THREE.Object3D();
+	let i;
+	let moveRMax = 0.1;
+	i = getDwellingSphere(getRandomInt(1));
+	i.position.set(end.x + getRandomPosNegFloat(moveRMax), end.y + getRandomPosNegFloat(moveRMax*0.07), end.z + getRandomPosNegFloat(moveRMax));
+    o.add(i);
+	i = getDwellingSphere(getRandomInt(0));
+	i.position.set(end.x + getRandomPosNegFloat(moveRMax), end.y + getRandomPosNegFloat(moveRMax*0.07), end.z + getRandomPosNegFloat(moveRMax));
+    o.add(i);
+	i = getDwellingSphere(getRandomInt(2));
+	i.position.set(end.x + getRandomPosNegFloat(moveRMax), end.y + getRandomPosNegFloat(moveRMax*0.07), end.z + getRandomPosNegFloat(moveRMax));
+    o.add(i);
+	i = getDwellingSphere(getRandomInt(2));
+	i.position.set(end.x + getRandomPosNegFloat(moveRMax), end.y + getRandomPosNegFloat(moveRMax*0.07), end.z + getRandomPosNegFloat(moveRMax));
+    o.add(i);
+	let g = new THREE.Geometry();
+	g.vertices.push(s, e);
+    let c = new THREE.Line( g, new THREE.LineBasicMaterial({ color: 0x000000 }) );
+    o.add(c);
+    o.name = "Dwelling_4";
+    o.start =  () => { return s; }
+    o.end = () => { return e; }
+    return o;
+}
+
+
+function Dwelling_6(start, end) {
+	let s = start;
+	let e = end;
+	let o = new THREE.Object3D();
+	let i;
+	let moveRMax = 0.1;
+	i = getDwellingSphere(getRandomInt(1));
+	i.position.set(end.x + getRandomPosNegFloat(moveRMax), end.y + getRandomPosNegFloat(moveRMax*0.07), end.z + getRandomPosNegFloat(moveRMax));
+    o.add(i);
+	i = getDwellingSphere(getRandomInt(0));
+	i.position.set(end.x + getRandomPosNegFloat(moveRMax), end.y + getRandomPosNegFloat(moveRMax*0.07), end.z + getRandomPosNegFloat(moveRMax));
+    o.add(i);
+	i = getDwellingSphere(getRandomInt(2));
+	i.position.set(end.x + getRandomPosNegFloat(moveRMax), end.y + getRandomPosNegFloat(moveRMax*0.07), end.z + getRandomPosNegFloat(moveRMax));
+    o.add(i);
+	i = getDwellingSphere(getRandomInt(2));
+	i.position.set(end.x + getRandomPosNegFloat(moveRMax), end.y + getRandomPosNegFloat(moveRMax*0.07), end.z + getRandomPosNegFloat(moveRMax));
+    o.add(i);
+	i = getDwellingSphere(getRandomInt(2));
+	i.position.set(end.x + getRandomPosNegFloat(moveRMax), end.y + getRandomPosNegFloat(moveRMax*0.07), end.z + getRandomPosNegFloat(moveRMax));
+    o.add(i);
+	i = getDwellingSphere(getRandomInt(2));
+	i.position.set(end.x + getRandomPosNegFloat(moveRMax), end.y + getRandomPosNegFloat(moveRMax*0.07), end.z + getRandomPosNegFloat(moveRMax));
+    o.add(i);
+	let g = new THREE.Geometry();
+	g.vertices.push(s, e);
+    let c = new THREE.Line( g, new THREE.LineBasicMaterial({ color: 0x000000 }) );
+    o.add(c);
+    o.name = "Dwelling_4";
+    o.start =  () => { return s; }
+    o.end = () => { return e; }
+    return o;
 }
 
 
@@ -95,82 +157,21 @@ function StiltedTurtle(start, end) {
 
 function RuleSetLine_2D_45Turn() {
 	this.run = (i, parent, turtle) => 	{
+									let lengthMul = 0.9;
 									let r = [];
 									let c;
-								    let axis = new THREE.Vector3( 1, 0, 0 );
-								    let angle = Math.PI / 4;
+								    let axis = new THREE.Vector3( 0, 1, 0 );
+								    let angle = Math.PI / 7;
 								    let src = parent.end().clone();
 								    let v1 = parent.end().clone().sub(parent.start());
 								    v1.applyAxisAngle( axis, angle );
-								    v1.multiplyScalar(0.5);
+								    v1.multiplyScalar(lengthMul);
 								    let e1 = parent.end().clone().add(v1);
 									c = new turtle(src, e1);
 								    r.push(c);
 								    let v2 = parent.end().clone().sub(parent.start());
 								    v2.applyAxisAngle( axis, -angle );
-								    v2.multiplyScalar(0.5);
-								    let e2 = parent.end().clone().add(v2);
-									c = new turtle(src, e2);
-								    r.push(c);
-									return r;
-								}
-}
-
-
-function RuleSetLine_3D_45Turn() {
-	this.run = (i, parent, turtle) => 	{
-									let r = [];
-									let c;
-								    let axis;
-								    axis = new THREE.Vector3( 1, 0, 0 )
-								    let angle = Math.PI / 4;
-								    let src = parent.end().clone();
-								    let v1 = parent.end().clone().sub(parent.start());
-								    v1.applyAxisAngle( axis, angle );
-								    v1.multiplyScalar(0.5);
-								    let e1 = parent.end().clone().add(v1);
-									c = new turtle(src, e1);
-								    r.push(c);
-								    let v2 = parent.end().clone().sub(parent.start());
-								    v2.applyAxisAngle( axis, -angle );
-								    v2.multiplyScalar(0.5);
-								    let e2 = parent.end().clone().add(v2);
-									c = new turtle(src, e2);
-								    r.push(c);
-								    axis = new THREE.Vector3( 0, 0, 1 );
-								    let v3 = parent.end().clone().sub(parent.start());
-								    v3.applyAxisAngle( axis, angle );
-								    v3.multiplyScalar(0.5);
-								    let e3 = parent.end().clone().add(v3);
-									c = new turtle(src, e3);
-								    r.push(c);
-								    let v4 = parent.end().clone().sub(parent.start());
-								    v4.applyAxisAngle( axis, -angle );
-								    v4.multiplyScalar(0.5);
-								    let e4 = parent.end().clone().add(v4);
-									c = new turtle(src, e4);
-								    r.push(c);
-									return r;
-								}
-}
-
-
-function RuleSetLine_2D_23Turn() {
-	this.run = (i, parent, turtle) => 	{
-									let r = [];
-									let c;
-								    let axis = new THREE.Vector3( 1, 0, 0 );
-								    let angle = Math.PI / 8;
-								    let src = parent.end().clone();
-								    let v1 = parent.end().clone().sub(parent.start());
-								    v1.applyAxisAngle( axis, angle );
-								    v1.multiplyScalar(0.5);
-								    let e1 = parent.end().clone().add(v1);
-									c = new turtle(src, e1);
-								    r.push(c);
-								    let v2 = parent.end().clone().sub(parent.start());
-								    v2.applyAxisAngle( axis, -angle );
-								    v2.multiplyScalar(0.5);
+								    v2.multiplyScalar(lengthMul);
 								    let e2 = parent.end().clone().add(v2);
 									c = new turtle(src, e2);
 								    r.push(c);
@@ -230,16 +231,16 @@ function ThreeBasicSys() {
 	this.d = 1;
 
 	this.axiom = function() {
-								let c = new currentTurtleType(new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, this.d, 0 ));
+								let c = new currentTurtleType(new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, -this.d ));
 								return [c];
 							};
 
 	this.setTurtle = function (turtleName) {
 		switch(turtleName) {
-			case 'Straight Line': { currentTurtleType = LineTurtle; break; }
-			case 'Stilted Line': { currentTurtleType = StiltedTurtle; break; }
-			case 'Bubble Line': { currentTurtleType = BubbleLineTurtle; break; }
-			default: { currentTurtleType = LineTurtle; }
+			case 'Dwelling_3': { currentTurtleType = Dwelling_3; break; }
+			case 'Dwelling_4': { currentTurtleType = Dwelling_4; break; }
+			case 'Dwelling_6': { currentTurtleType = Dwelling_6; break; }
+			default: { currentTurtleType = Dwelling_4; }
 		}
 	}
 
@@ -247,17 +248,15 @@ function ThreeBasicSys() {
 		// console.log(rulesetName);
 		switch(rulesetName) {
 			case '2-45': { ruleSet = new RuleSetLine_2D_45Turn(); break; }
-			case '4-45': { ruleSet = new RuleSetLine_3D_45Turn(); break; }
-			case '2-23': { ruleSet = new RuleSetLine_2D_23Turn(); break; }
-			default: { ruleSet = new RuleSetLine_2D_45Turn(); }
+			default: { ruleSet = new RuleSetLine_3D_45Turn(); }
 		}
 	}
 
 	this.getCurrentTurtleType = function() { return currentTurtleType; }
 	this.getRuleSet = function() { return ruleSet; }
 
-	this.setTurtle(StiltedTurtle);
-	this.setRuleset(RuleSetLine_3D_45Turn);
+	this.setTurtle('Dwelling_4');
+	this.setRuleset('2-45');
 }
 
 
@@ -290,8 +289,8 @@ function L3DApp() {
 		scene = new THREE.Scene();
 		controls = new THREE.OrbitControls( camera );
 
-		let gridHelper = new THREE.GridHelper( 20, 100 );
-		scene.add( gridHelper );
+		// let gridHelper = new THREE.GridHelper( 20, 100 );
+		// scene.add( gridHelper );
 
 		renderer = new THREE.WebGLRenderer( { antialias: true } );
 		renderer.setSize( window.innerWidth, window.innerHeight );
